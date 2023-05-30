0f5c462cf83fa731065175ebeee77d78e736a381
Lauren Seward <lauren.unal@gmail.com>
2023-05-30 15:36:33 -0500

    Add mock data for get top stories fetch

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,11 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Header from '../Header/Header';
+import SearchBar from '../SearchBar/SearchBar';
+import ArticleList from '../ArticleList/ArticleList';
+import ArticleDetail from '../ArticleDetail/ArticleDetail';
+import mockData from '../../mockData';
+
 
 const App = () => {
   return (
