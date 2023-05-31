408939f0af6d2845d4a48f7e0695a47dbd9a90a3
Lauren Seward <lauren.unal@gmail.com>
2023-05-30 19:54:02 -0500

    Add basic styling to search bar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -4,6 +4,7 @@ const SearchBar = () => {
   return (
     <div className='search-bar'>
       <input type='text' placeholder='Search for articles..' />
+      <button className='submit-search-btn'>SEARCH</button>
     </div>
   )
 }
