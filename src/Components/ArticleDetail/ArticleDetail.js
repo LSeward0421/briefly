0686254c4b45cce9a3897f83bb2eb7421bc9930d
Lauren Seward <lauren.unal@gmail.com>
2023-05-30 20:37:45 -0500

    Add styling to article details

--- src/Components/ArticleDetail/ArticleDetail.js
+++ src/Components/ArticleDetail/ArticleDetail.js
@@ -1,16 +1,27 @@
-import './ArticleDetail.css';
+import { useHistory, useParams } from "react-router-dom";
+import "./ArticleDetail.css";
 
+const ArticleDetail = ({ articles }) => {
+  const { id } = useParams();
+  const history = useHistory();
+  const article = articles[id];
 
-const ArticleDetail = () => {
   return (
-    <div className='article-details'>
-      <h2>Article Details</h2>
-      <p>Description</p>
-      <img src='image src' alt='image alt' />
-      <p>article content..</p>
-      <p>Source: article source</p>
+    <div className="article-details">
+      {article ? (
+        <>
+          <h2>{article.title}</h2>
+          <img src={article.urlToImage} alt={article.title} />
+          <p>{new Date(article.publishedAt).toLocaleDateString()}</p>
+          <p>{article.content.replace(/\[\+\d+ chars\]$/g, '')} <a href={article.url} target="_blank" rel="noreferrer">Read the full story.</a></p>
+          <p>Source: {article.source.name}</p>
+        </>
+      ) : (
+        <div>Article not found</div>
+      )}
+      <button onClick={() => history.push("/")}>Back to List</button>
     </div>
-  )
-}
+  );
+};
 
 export default ArticleDetail;
