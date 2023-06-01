18e97f863ab3ca56df107e12ff601444fb89b71f
Lauren Seward <lauren.unal@gmail.com>
2023-05-31 21:47:54 -0500

    Update styling for cohesive look

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -11,7 +11,7 @@ const fetchArticles = async (query = null) => {
     const response = await fetch(url);
 
     if (!response.ok) {
-      throw new Error(`Status: ${response.status}`);
+      throw new Error(` Status: ${response.status}`);
     }
 
     const data = await response.json();
